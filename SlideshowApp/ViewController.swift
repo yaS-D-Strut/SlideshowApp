620816549f3703522d7ecf04aeb4fdf744b6e61d
yasuaki.osada <dice.worker@gmail.com>
2019-09-01 22:11:02 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Yasuaki Osada on 2019/09/01.
+//  Copyright © 2019 Yasuaki Osada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
